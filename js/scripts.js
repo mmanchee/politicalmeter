b9f110af1c9050f2e97c9d863db8b428543d6d32
Alma <a.q.frankenstein@gmail.com>
2020-08-13 11:51:49 -0700

    refactor
    co-authored-by: Mike Manchee <mikemanchee@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,33 @@
+function orients(total) {
+  
+  let orient;
+
+  if (total <= 2) {
+    orient = "liberal";
+  } else if (total >= 6) {
+    orient = "conservative";
+  } else {
+    orient = "moderate";
+  }
+  return orient;
+}
+
 $(document).ready(function(){
   $("form#survey").submit(function(event) {
     const moneyVal = parseInt($("input:radio[name=money]:checked").val());
     const libertyVal = parseInt($("input:radio[name=liberty]:checked").val());
     const total = moneyVal + libertyVal;
+
+    let orientation = orients(total);
     
-    let orientation;
-
-    if (total <= 2) {
-      orientation = "liberal";
-    } else if (total >= 6) {
-      orientation = "conservative";
-    } else {
-      orientation = "moderate";
-    }
-
     $("#party").text(orientation);
     $("#orientation").show();
 
     event.preventDefault();
   });
 });
+
+
 
 
 
